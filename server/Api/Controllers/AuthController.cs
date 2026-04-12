dc7c254a06fe4c36034eadae697c6ebe2c8e0efd
odrymark <odrymark@gmail.com>
2026-04-12 10:04:32 +0200

    Issue with empty catch block

--- server/Api/Controllers/AuthController.cs
+++ server/Api/Controllers/AuthController.cs
@@ -85,7 +85,9 @@ public class AuthController : ControllerBase
                 await _service.Logout(refreshToken);
             }
             catch (KeyNotFoundException)
-            {}
+            {
+                //Clear Cookies
+            }
         }
 
         Response.Cookies.Delete("jwt");
